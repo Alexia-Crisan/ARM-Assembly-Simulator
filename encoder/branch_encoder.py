fc536a4f1c55ef06e8087afab55443eeb636c031
Alexia-Crisan <150911612+Alexia-Crisan@users.noreply.github.com>
2025-10-11 16:34:08 +0300

    encoder for branch

---
+++ encoder/branch_encoder.py
@@ -0,0 +1,23 @@
+"""
+Branch (B): cond|101|offset24
+"""
+
+from typing import Dict
+from encoder import register_to_number, encode_immediate_value
+
+COND_ALWAYS = 0b1110 << 28 # always flag: 0b1110
+
+def encode_branch(label: str, current_place: int, labels: Dict[str, int]) -> int:
+    if label not in labels:
+        raise ValueError(f"Unknown label: {label}")
+    
+    target = labels[label]
+
+    offset = (target - (current_place + 8)) >> 2
+ 
+    if not (- (1 << 23) <= offset < (1 << 23)):
+        raise ValueError("Branch offset out of range")
+    
+    offset24 = offset & 0xFFFFFF
+    machine_instruction = COND_ALWAYS | (0b101 << 25) | offset24
+    return machine_instruction
